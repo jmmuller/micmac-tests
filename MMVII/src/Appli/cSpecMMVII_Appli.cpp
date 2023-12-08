604d27ae18573c433d656d0616f77da0bcea16c3
deseilligny <marc.pierrot-deseilligny@ign.fr>
2023-12-08 12:21:13 +0100

    In pipe areo

--- MMVII/src/Appli/cSpecMMVII_Appli.cpp
+++ MMVII/src/Appli/cSpecMMVII_Appli.cpp
@@ -235,6 +235,7 @@ std::vector<cSpecMMVII_Appli *> & cSpecMMVII_Appli::InternVecAll()
         TheVecAll.push_back(&TheSpec_ImportTiePMul);
         TheVecAll.push_back(&TheSpec_ImportMesImGCP);
         TheVecAll.push_back(&TheSpec_ComputeTriangleDeformation);
+        TheVecAll.push_back(&TheSpec_ImportTiePMul);
 
         std::sort(TheVecAll.begin(),TheVecAll.end(),CmpCmd);
    }
