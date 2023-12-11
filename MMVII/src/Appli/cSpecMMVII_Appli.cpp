b911efbe83e2b4470b76e3e9da8ba6f62aca75ef
deseilligny <marc.pierrot-deseilligny@ign.fr>
2023-12-11 15:06:06 +0100

    In Process AerialBundle

--- MMVII/src/Appli/cSpecMMVII_Appli.cpp
+++ MMVII/src/Appli/cSpecMMVII_Appli.cpp
@@ -233,6 +233,7 @@ std::vector<cSpecMMVII_Appli *> & cSpecMMVII_Appli::InternVecAll()
         TheVecAll.push_back(&TheSpec_RandomGeneratedDelaunay);
         TheVecAll.push_back(&TheSpec_ComputeTriangleDeformation);
         TheVecAll.push_back(&TheSpec_ImportTiePMul);
+        TheVecAll.push_back(&TheSpec_ImportMesImGCP);
 
         std::sort(TheVecAll.begin(),TheVecAll.end(),CmpCmd);
    }
